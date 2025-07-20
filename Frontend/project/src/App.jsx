b4eef0a8f043eb231188153b04d84d4afa79b157
ganeshborole <ganeshborole73@gmail.com>
2025-07-20 17:41:14 +0530

    Yt-clone

---
+++ Frontend/project/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
+import Home from "./components/Home";
+import PlayingVideo from "./components/PlayingVideo";
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/search/:searchQuery" element={<Search />} />
+        <Route path="/video/:id" element={<PlayingVideo />} />
+      </Routes>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/project/src/components/PlayingVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlayingVideo() {
+  return (
+    <div className='mt-35'>PlayingVideo</div>
+  )
+}
+
+export default PlayingVideo
